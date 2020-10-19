73e63497c373516312882832f839625d3ae48928
nikdev <nikdev1380@gmail.com>
2020-10-18 21:27:03 -1000

    add header blog

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<div>
+<p>
+footer
+</p>
+</div>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,42 @@
+<header class="shadow-sm">
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-8" id="header-menu">
+              <ul class="list-unstyled p-0 m-0 text-right">
+                  <li>
+                    <a href="{{url('/')}}"class="d-inline-block float-right text-decoration-none p-3 font-18  BYekan">
+                    خانه
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="{{url('/')}}"class="d-inline-block float-right text-decoration-none p-3 font-18  BYekan">
+                    درباره ما
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="{{url('/')}}"class="d-inline-block float-right text-decoration-none p-3 font-18  BYekan">
+                    تماس با ما
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="{{url('/')}}"class="d-inline-block float-right text-decoration-none p-3 font-18  BYekan">
+                    ورود
+                    </a>
+                  </li>
+
+
+              </ul>
+          </div>
+
+          <div class="col-lg-4" id="header-logo">
+          <a href="">
+         <img alt="kouventa" src="https://kouventa.ir/images/Kouventa-300-151.png" class="w-50 p-2">
+         
+          </a>
+          </div>
+      </div>
+</div>
+</header>
